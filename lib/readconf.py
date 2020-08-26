984742388be3644b0197d249d82805198c9f48b7
kezhe <554011540@qq.com>
2020-08-26 11:28:23 +0800

    柯哲；1、新增读取配置模块readconf；2、修改被叫号码可以设置成多个

---
+++ lib/readconf.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# coding=utf-8
+import os
+from configparser import ConfigParser
+
+def ReadConfig():
+    # 获取config.ini文件的位置
+    config_file_path = os.path.join(os.path.abspath('..'), 'conf','config.ini')
+    config = ConfigParser()
+    config.read(config_file_path,encoding='utf-8')
+    return config
--- lib/wap1.py
+++ lib/wap1.py
@@ -3,20 +3,23 @@
 import os
 import datetime
 import time
+from readconf import ReadConfig
 
 
 def file_monitor():
+    cf = ReadConfig()
     # 设置主叫号码
-    org_phone = '6009'
-    # 设置被叫号码
-    des_phone = '186XXXXXXXX'
+    org_phone = cf.get('phonenumber', 'org_phone')
+    # 设置被叫号码(存储成列表)
+    des_phones = cf.get('phonenumber', 'des_phone').split(',')
     # 设置读取文件数目路径
-    # path_read = r'/root/ceshi'
-    path_read = r'D:/测试'
+    path_read = cf.get('filepath', 'path_read')
     # 设置通知日志存储路径
-    path_log = 'D:/测试1'
+    path_log = cf.get('filepath', 'path_log')
     # 设置异常退信息存储路径
-    path_error = 'D:/测试2'
+    path_error = cf.get('filepath', 'path_error')
+    # 设置程序间隔时间
+    sleep_time = int(cf.get('interval', 'sleeptime'))
 
     # 循环判断程序
     while True:
@@ -38,29 +41,32 @@ def file_monitor():
                 error_text = '积压文件数量为{0}个'.format(num)
                 # 设置日志文件名时间
                 file_error = time_now.strftime('%Y%m%d%H%M%S')
-                # 设置文本内容
-                text = "{0},{1},{2},{3}".format(time_now.strftime('%Y-%m-%d %H:%M:%S'), error_text, org_phone,
-                                                des_phone)
 
                 with open(r'{0}/{1}.txt'.format(path_log, file_error), 'w')as f:
-                    f.write(text)
+                    for des_phone in des_phones:
+                        # 设置文本内容
+                        text = "{0},{1},{2},{3}\n".format(time_now.strftime('%Y-%m-%d %H:%M:%S'), error_text, org_phone,
+                                                          des_phone)
+                        f.write(text)
 
             # 设置休眠时间
-            time.sleep(60)
+            time.sleep(sleep_time)
 
         except Exception as e:
             time_break = datetime.datetime.now()
             file_break = time_break.strftime('%Y%m%d%H%M%S')
-            text_break = '{0},监控程序异常退出,{1},{2}'.format(time_break.strftime('%Y-%m-%d %H:%M:%S'), org_phone,
-                                                       des_phone)
+
             # 生成监控程序退出告警
             with open(r'{0}/{1}.txt'.format(path_log, file_break), 'w')as f:
-                f.write(text_break)
+                for des_phone in des_phones:
+                    text_break = '{0},监控程序异常退出,{1},{2}\n'.format(time_break.strftime('%Y-%m-%d %H:%M:%S'), org_phone,
+                                                               des_phone)
+                    f.write(text_break)
             # 存储异常信息
             with open(r'{0}/{1}.txt'.format(path_error, 'error' + file_break), 'w')as f:
                 f.write(str(e))
-            break
+            print(e)
 
-if __ == '__main__':
-    file___namemonitor()
-    #eeeee
+
+if __name__ == '__main__':
+    file_monitor()
