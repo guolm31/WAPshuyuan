92cac5e7645f6848acb287cdca0ec9070cf71e02
Guo liming <18607181232@wo.cn>
2020-08-27 15:56:43 +0800

    guoliming git cmd111

--- bin/test.py
+++ bin/test.py
@@ -1,34 +1,26 @@
+#!/usr/bin/python
+# coding=utf-8
+
 import lib.logger as logger
 import logging
 import lib.smpp as smpp
+from lib.readconf import ReadConfig
 
-class Test():
-    def __init__(self):
-        logg=logger.Logger()
-        logging.debug('this is a init debug from test.py')
-    def do(self):
-        logging.debug('this is a do debug from test.py')
-
-
-
-
-test1=Test()
-test1.do()
-
-def main(msg,msisdns):
-    cfg = {
-        'host' : '10.222.45.24', 'port' : 5016 ,
-        'user' : 'YJDX', 'passwd' : 'YJdx!2'
-    }
-    logging.error(msg)
-
-    S = smpp.SmppSendMSG(cfg['host'], cfg['port'], cfg['user'], cfg['passwd'])
-    S.sendgroup(msg,msisdns,tsleep=0.2)  # 延时根据短信中心每秒限制设定
+def send(msg):
+    logg = logger.Logger()
+    cf = ReadConfig()
+    smpp_host = cf.get('smpp', 'host')
+    smpp_port = cf.get('smpp', 'port')
+    smpp_user = cf.get('smpp', 'user')
+    smpp_passwd = cf.get('smpp', 'passwd')
+    org_phone = cf.get('phonenumber', 'org_phone')
+    # 设置被叫号码(存储成列表)
+    des_phones = cf.get('phonenumber', 'des_phone').split(',')
+    S = smpp.SmppSendMSG(smpp_host, smpp_port, smpp_user, smpp_passwd)
+    S.sendsms(org_phone,msg,des_phones,tsleep=0.2)  # 延时根据短信中心每秒限制设定
 
 if __name__ == "__main__":
-    msg_test = ("你好hello123")
-    m_list = []
-    m_list.append('18607181232')
-    main(msg_test,m_list)
+    send("你好")
+    logging.error("发送成功")
 #dddd
 
