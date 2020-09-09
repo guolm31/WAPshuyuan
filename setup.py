5cb78611c1c0614ae504fdf28cfa57c746527f18
Guo liming <18607181232@wo.cn>
2020-09-09 15:54:41 +0800

    guoliming git cmd111

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="WAPshuyuan",
+    version="1.0",
+    author="ywpt",
+    author_email="ywpt@qq.com",
+    description="话单监控",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="https://github.com/guolm31/WAPshuyuan",
+    packages=setuptools.find_packages(),
+
+)
