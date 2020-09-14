996ffdb46a50baae8de6df235759cf6a413924e6
kezhe <554011540@qq.com>
2020-09-14 10:18:45 +0800

    kezhe 优化短信告警内容，加上本机IP

--- bin/filemain.py
+++ bin/filemain.py
@@ -11,28 +11,30 @@ from lib.fileerror import FileError
 from lib.fileftp import FileFtp
 
 cf = ReadConfig()
-# 设置主叫号码
+# 获取主叫号码
 org_phone = cf.get('phonenumber', 'org_phone')
-# 设置被叫号码(存储成列表)
+# 获取被叫号码(存储成列表)
 des_phones = cf.get('phonenumber', 'des_phone').split(',')
-# 设置读取文件数目路径
+# 获取读取文件数目路径
 path_read = cf.get('filepath', 'path_read')
-# 设置通知日志存储路径
+# 获取通知日志存储路径
 path_log = cf.get('filepath', 'path_log')
-# 设置异常退信息存储路径
+# 获取异常退信息存储路径
 path_error = cf.get('filepath', 'path_error')
-# 设置程序间隔时间
+# 获取程序间隔时间
 sleep_time = cf.getint('interval', 'sleeptime')
-# 设置文件告警阈值
+# 获取文件告警阈值
 file_num = cf.getint('filenumber', 'filenum')
-# 设置ftp ip
+# 获取ftp ip
 ftp_ip = cf.get('ftp','ftp_ip')
-# 设置ftp 用户
+# 获取ftp 用户
 ftp_user = cf.get('ftp','ftp_user')
-# 设置ftp 密码
+# 获取ftp 密码
 ftp_passwd = cf.get('ftp','ftp_passwd')
-# 设置ftp 文件保存路径
+# 获取ftp 文件保存路径
 ftp_path = cf.get('ftp','ftp_path')
+# 获取本机ip
+local_ip = cf.get('localip','local_ip')
 
 if __name__ == '__main__':
     while True:
@@ -41,12 +43,12 @@ if __name__ == '__main__':
             num = FileMonitor(path_read)
             if num > file_num:
                 # 生成通知文件
-                FileCreat(org_phone,des_phones,path_log,num)
+                FileCreat(org_phone,des_phones,path_log,num,local_ip)
                 # 上传通知文件
                 FileFtp(ftp_ip,ftp_user,ftp_passwd,ftp_path,path_log)
                 # 休眠
                 time.sleep(sleep_time)
         except Exception as e:
             # 生成异常文件，并保存错误日志
-            FileError(e,path_error,des_phones,org_phone,path_log)
+            FileError(e,path_error,des_phones,org_phone,path_log,local_ip)
             time.sleep(sleep_time)
--- lib/fileerror.py
+++ lib/fileerror.py
@@ -4,13 +4,14 @@
 import datetime
 from lib.readconf import ReadConfig
 
-def FileError(e,path_error,des_phones,org_phone,path_log):
+def FileError(e,path_error,des_phones,org_phone,path_log,localip):
     '''
     此方法用于报错的处理
     :param e: 报错的内容
     :param path_error: 报错的存储路径
     :param des_phones: 通知短信的被叫号码
     :param path_log: 需要ftp的文件路径
+    :param localip:本机ip
     '''
     time_break = datetime.datetime.now()
     file_break = time_break.strftime('%Y%m%d%H%M%S')
@@ -18,7 +19,7 @@ def FileError(e,path_error,des_phones,org_phone,path_log):
     # 生成监控程序 退出告警
     with open(r'{0}/{1}.txt'.format(path_log, file_break), 'w')as f:
         for des_phone in des_phones:
-            text_break = '{0},监控程序异常,{1},{2}\n'.format(time_break.strftime('%Y-%m-%d %H:%M:%S'), org_phone,
+            text_break = '{0},{1}监控程序异常,{2},{3}\n'.format(time_break.strftime('%Y-%m-%d %H:%M:%S'),localip, org_phone,
                                                        des_phone)
             f.write(text_break)
     # 存储异常信息
